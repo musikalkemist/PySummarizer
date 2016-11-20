70454abfb961d2a8d311e0d9ae4ae78ac067e477
musikalkemist <velardovalerio@gmail.com>
2016-11-20 15:16:31 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'pysummarizer',
   packages = ['pysummarizer'], 
-  version = '0.2',
+  version = '0.3',
   description = 'Library for automatic text summarization',
   author = 'Valerio Velardo',
   author_email = 'velardovalerio@gmail.com',
   url = 'https://github.com/musikalkemist/PySummarizer', 
-  download_url = 'https://github.com/musikalkemist/PySummarizer/tarball/0.2.1', 
+  download_url = 'https://github.com/musikalkemist/PySummarizer/tarball/0.3.1', 
   keywords = ['summarization', 'nlp', 'automatic summarization', 'information retrieval'], 
   classifiers = [],
 )

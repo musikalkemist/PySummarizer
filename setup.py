7ebff0facbad9ccb30ed163c279ac93f08d4228c
valerio <valerio@debian>
2016-11-20 11:50:18 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'pysummarize',
+  packages = ['pysummarize'], 
+  version = '0.1',
+  description = 'A library for automatic text classification',
+  author = 'Valerio Velardo',
+  author_email = 'velardovalerio@gmail.com',
+  url = 'https://github.com/musikalkemist/PySummarize', 
+  download_url = 'https://github.com/musikalkemist/PySummarize/tarball/0.1', 
+  keywords = ['summarization', 'nlp', 'automatic summarization'], 
+  classifiers = [],
+)
